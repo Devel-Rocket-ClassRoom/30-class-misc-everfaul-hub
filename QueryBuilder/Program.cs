bd5510b0f964a30f708ae8442e2490505de0fb73
JUYOUNGCHAN <everfaul@gmail.com>
2026-03-02 18:14:23 +0900

    blackjack 예시4

--- QueryBuilder/Program.cs
+++ QueryBuilder/Program.cs
@@ -1,12 +1,54 @@
 using System;
-using System.Collections.ObjectModel;
+
+using System.Text;
 
 public class QueryBuilder
 {
-    private string _message = "";
+    private string selectClause = "";
+    private string fromClause = "";
+    private string whereClause = "";
+    private string orderByClause = "";
 
-    public QueryBuilder Select(string text)
+    public QueryBuilder Select(string columns)
     {
+        selectClause = $"SELECT {columns}";
+        return this;
+    }
 
+    public QueryBuilder From(string table)
+    {
+        fromClause = $"FROM {table}";
+        return this;
+    }
+
+    public QueryBuilder Where(string condition)
+    {
+        whereClause = $"WHERE {condition}";
+        return this;
+    }
+
+    public QueryBuilder OrderBy(string column)
+    {
+        orderByClause = $"ORDER BY {column}";
+        return this;
+    }
+
+    public string Build()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        if (!string.IsNullOrEmpty(selectClause))
+            sb.Append(selectClause + " ");
+
+        if (!string.IsNullOrEmpty(fromClause))
+            sb.Append(fromClause + " ");
+
+        if (!string.IsNullOrEmpty(whereClause))
+            sb.Append(whereClause + " ");
+
+        if (!string.IsNullOrEmpty(orderByClause))
+            sb.Append(orderByClause);
+
+        return sb.ToString().Trim();
     }
 }
